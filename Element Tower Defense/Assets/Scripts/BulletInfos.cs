637616e64bd9f4dd6fd50ce07d95be9893483b45
unknown <patrick.nibbia@gmail.com>
2021-05-14 15:32:11 +0200

    feat: add scripts for bullets and enemy stats

---
+++ Element Tower Defense/Assets/Scripts/BulletInfos.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletInfos : MonoBehaviour
+{
+
+    private float damage = 10;
+    private Elements bulletElement = Elements.NEUTRAL;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"Bullet {bulletElement} says hello with a damage of: {damage}");
+        switch (bulletElement)
+        {
+            case Elements.ELECTRO:
+                print("Case Electro");
+                this.GetComponent<Renderer>().material.SetColor("_Color", Color.magenta);
+                break;
+            case Elements.FIRE:
+                print("Case Fire");
+                this.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+                break;
+            case Elements.WATER:
+                print("Case Water");
+                this.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+                break;
+            default:
+                print("An almighty element that is bursting with neutrality. (This is a placeholder element and shouldn't actually appear in the game.");
+                this.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("HIT!");
+            if (!other.gameObject.GetComponent<EnemyStats>().GetMonsterStatus())
+            {
+                other.gameObject.GetComponent<EnemyStats>().TakeDamage(bulletElement, damage);
+            }
+        }
+        Destroy(this.gameObject);
+    }
+
+    public void SetBulletElementType(Elements elementType)
+    {
+        bulletElement = elementType;
+    }
+
+    public void SetBulletDamage(int towerLv)
+    {
+        damage *= towerLv;
+    }
+}
---
+++ Element Tower Defense/Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    private int level;
+    private float health = 100.00F;
+    private bool isDead = false;
+    private Elements monsterElement = Elements.NEUTRAL;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set monster element
+        monsterElement = (Elements)Random.Range(0, 4);
+        SetMonsterElement(monsterElement);
+        //Renderer monsterRenderer = this.GetComponent<Renderer>();
+        //monsterRenderer.material.SetColor("_Color", Color.red);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetDeath();
+    }
+
+    public void SetDeath()
+    {
+        if (isDead)
+        {
+            Debug.Log($"Enemy health: {health}");
+            // Increase player coins
+            Destroy(this.gameObject);
+        }
+    }
+
+    public bool GetMonsterStatus()
+    {
+        return isDead;
+    }
+
+    public void TakeDamage(Elements elementType, float damage)
+    {
+        if (monsterElement == elementType)
+        {
+            print("Normal Damage");
+            health -= damage;
+        } else if(monsterElement == elementType)
+        {
+            print("Immune");
+        } else if (monsterElement == Elements.ELECTRO && elementType == Elements.WATER)
+        {
+            health -= CalcDamage(damage, true);
+        } else if (monsterElement == Elements.ELECTRO && elementType == Elements.FIRE)
+        {
+            health -= CalcDamage(damage, false);
+        }
+        else if (monsterElement == Elements.FIRE && elementType == Elements.ELECTRO)
+        {
+            health -= CalcDamage(damage, true);
+        }
+        else if (monsterElement == Elements.FIRE && elementType == Elements.WATER)
+        {
+            health -= CalcDamage(damage, false);
+        }
+        else if (monsterElement == Elements.WATER && elementType == Elements.FIRE)
+        {
+            health -= CalcDamage(damage, true);
+        }
+        else if (monsterElement == Elements.WATER && elementType == Elements.ELECTRO)
+        {
+            health -= CalcDamage(damage, false);
+        }
+    }
+
+    public void SetDamageToPlayer()
+    {
+        // Decrease Player healh
+        Destroy(this.gameObject);
+    }
+
+    private float CalcDamage(float damage, bool isEffective)
+    {
+        if (isEffective)
+        {
+            return damage * 2;
+        } else
+        {
+            return damage / 2;
+        }
+    }
+
+    private void SetMonsterElement(Elements elementType)
+    {
+        switch (elementType)
+        {
+            case Elements.NEUTRAL:
+                print("Case Electro");
+                this.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+                break;
+            case Elements.ELECTRO:
+                print("Case Electro");
+                this.GetComponent<Renderer>().material.SetColor("_Color", Color.magenta);
+                break;
+            case Elements.FIRE:
+                print("Case Fire");
+                this.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+                break;
+            case Elements.WATER:
+                print("Case Water");
+                this.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+                break;
+            default:
+                print("Default case (Somthing bad happend)");
+                this.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+                break;
+        }
+    }
+}
