84c437904fc8d330b2849649c0e9ec1fadaf13ec
unknown <patrick.nibbia@gmail.com>
2021-05-15 13:09:40 +0200

    feat: add wavespawner

---
+++ Element Tower Defense/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform enemySpawnPoint;
+    private int waveID = 1;
+    private int difficultyMultiplyer = 2;
+    private float waitTime = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnWaveOverUI()
+    {
+        StartCoroutine(SpawnWave());
+    }
+
+    IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveID + (waveID * difficultyMultiplyer); i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(waitTime);
+        }
+        waveID++;
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, enemySpawnPoint.position, enemySpawnPoint.rotation);
+    }
+}
---
+++ Element Tower Defense/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public Transform waypointHolder;
+    public static Transform[] waypoints;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        waypoints = new Transform[waypointHolder.transform.childCount];
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            print($"Point {waypointHolder.transform.GetChild(i).name}");
+            waypoints[i] = waypointHolder.transform.GetChild(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static Transform[] GetWaypoints()
+    {
+        return waypoints;
+    }
+}
