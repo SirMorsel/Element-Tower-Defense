59869276b2d28b657bfe097b0aa05dcdbba54291
unknown <patrick.nibbia@gmail.com>
2021-05-29 20:09:48 +0200

    fix: init first selected tower bug

--- Element Tower Defense/Assets/Scripts/BuildManager.cs
+++ Element Tower Defense/Assets/Scripts/BuildManager.cs
@@ -21,6 +21,7 @@ public class BuildManager : MonoBehaviour
         towerUI = GameObject.Find("TowerUI");
         print($"TEST {towerUI}");
         towerToBuildBackUp = towerToBuild;
+        towerToBuild = null;
     }
 
     // Update is called once per frame
